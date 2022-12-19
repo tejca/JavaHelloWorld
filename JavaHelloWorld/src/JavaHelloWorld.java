4290a69dec9412b65b0a4eb4e86f6db490cd069f
tejca <121038258+tejca@users.noreply.github.com>
2022-12-19 15:47:10 +0100

    Add files via upload

---
+++ JavaHelloWorld/src/JavaHelloWorld.java
@@ -0,0 +1,11 @@
+public class JavaHelloWorld {
+    public static void main(String[] args) {
+
+        for ( int i = 0; i <=36; i = i +1) {
+
+            System.out.println("Hello World");
+
+        }
+
+    }
+}
